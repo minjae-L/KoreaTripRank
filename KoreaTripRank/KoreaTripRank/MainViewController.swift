fcd37ef5b17b846b9365d151f2f560f5c55d5277
이민재 <qlqlzbto@naver.com>
2024-11-02 23:38:48 +0900

    NavigationToolbar 지정

--- KoreaTripRank/KoreaTripRank/MainViewController.swift
+++ KoreaTripRank/KoreaTripRank/MainViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    func setUpNavigationToolbar() {
+        self.navigationController?.isToolbarHidden = false
+        
+        let appearance = UIToolbarAppearance()
+        appearance.backgroundColor = .orange
+        
+        let favoriteButton = UIBarButtonItem(image: UIImage(systemName: "star"), style: .done, target: self, action: #selector(favoriteButtonTapped))
+        let mainViewButton = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .done, target: self, action: #selector(mainViewButtonTapped))
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        self.navigationController?.toolbar.scrollEdgeAppearance = appearance
+        let items = [space, mainViewButton, space, space, favoriteButton, space]
+        self.toolbarItems = items
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        setUpNavigationToolbar()
     }
-
+    
+    @objc func favoriteButtonTapped() {
+        print("favoriteButton Tapped")
+    }
+    @objc func mainViewButtonTapped() {
+        print("mainViewButton Tapped")
+    }
 
 }
 
