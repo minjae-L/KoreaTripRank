82c3d161369bdc4ae0b7c67c13ad212c050d4baf
이민재 <qlqlzbto@naver.com>
2024-11-02 18:11:48 +0900

    프로젝트 파일 추가

---
+++ KoreaTripRank/KoreaTripRank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoreaTripRank
+//
+//  Created by 이민재 on 11/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
