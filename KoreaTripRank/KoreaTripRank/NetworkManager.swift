ea18680d9adb5b0838807881adec11c7a7ee125c
이민재 <qlqlzbto@naver.com>
2024-11-06 22:56:21 +0900

    NetworkManager 파일 생성

---
+++ KoreaTripRank/KoreaTripRank/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  KoreaTripRank
+//
+//  Created by 이민재 on 11/6/24.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    let url: String = ""
+    
+}
