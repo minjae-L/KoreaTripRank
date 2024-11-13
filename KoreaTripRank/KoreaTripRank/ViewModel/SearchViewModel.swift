42ed009fdb68522d978307177572aefe1775f3cf
이민재 <qlqlzbto@naver.com>
2024-11-13 22:15:43 +0900

    선택한 지역을 MapKit활용하여 위도 경도를 구하고, x y 값으로 변환하고 저장

--- KoreaTripRank/KoreaTripRank/ViewModel/SearchViewModel.swift
+++ KoreaTripRank/KoreaTripRank/ViewModel/SearchViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import MapKit
 
 protocol SearchViewModelDelegate: AnyObject {
     func addressSearching()
@@ -19,6 +20,11 @@ final class SearchViewModel {
     }
     var areaDatabase = AreaDatabase()
     weak var delegate: SearchViewModelDelegate?
+    var selectedSigungu: LocationDataModel?
+    var selectedLocationInfo: ConvertedLocationModel?
+    
+    var completer = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
     
     func filtering(text: String) {
         var output = [LocationDataModel]()
@@ -30,5 +36,39 @@ final class SearchViewModel {
             }
         }
         self.filteredAddressArray = output
+    }
+    private func search(for suggestionCompletion: MKLocalSearchCompletion) async throws {
+        let searchRequest = MKLocalSearch.Request(completion: suggestionCompletion)
+        searchRequest.region = MKCoordinateRegion(MKMapRect.world)
+        searchRequest.resultTypes = .address
+        
+        let localSearch = MKLocalSearch(request: searchRequest)
+        do {
+            let response = try await localSearch.start()
+            print("MapKit Search Success")
+            let lati = response.mapItems[0].placemark.coordinate.latitude
+            let long = response.mapItems[0].placemark.coordinate.longitude
+            var location = ConvertedLocationModel(lat: lati, lng: long)
+            location.convertGRID_GPS(mode: 0, lat_X: lati, lng_Y: long)
+            selectedLocationInfo = location
+            self.loadData()
+        } catch {
+            print("MapKit Search Error")
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func didSected(text: String, index: Int) {
+        completer.queryFragment = text
+        completer.resultTypes = .address
+        self.selectedSigungu = filteredAddressArray[index]
+        
+    }
+    
+    func getLocation(results: [MKLocalSearchCompletion]) {
+        Task {
+            try await self.search(for: results[0])
+        }
     }
 }
