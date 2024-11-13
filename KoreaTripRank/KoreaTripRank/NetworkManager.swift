c2d4440c916eb95820b0ccd1daf4449eca5e6f45
이민재 <qlqlzbto@naver.com>
2024-11-13 15:36:29 +0900

    URL 열거형에 따른 URLComponents 생성 / 해당 URL로 네트워크 통신

--- KoreaTripRank/KoreaTripRank/NetworkManager.swift
+++ KoreaTripRank/KoreaTripRank/NetworkManager.swift
@@ -44,17 +44,37 @@ class NetworkManager {
         self.decoder = decoder
     }
     
-    func fetchData<T: Decodable>(type: T.Type) async throws -> Result<T, AFError> {
+    func fetchData<T: Decodable>(type: T.Type) async throws -> T {
         guard let url = components.getURLComponents().url else {
-            print("invalidURL")
-            return .failure(AFError.invalidURL(url: ""))
+            throw(NetworkError.invalidURL)
         }
-        do {
-            let data = try await AF.request(url).serializingDecodable(T.self).value
-            return .success(data)
-        } catch {
-            return .failure(error as! AFError)
+        let request = AF.request(url)
+        
+        let response = await request.serializingDecodable(T.self).response
+        guard (response.response)?.statusCode == 200 else {
+            throw NetworkError.serverError(code: response.response?.statusCode ?? 0)
         }
+        
+        guard let data = response.value else {
+            throw NetworkError.decodingError
+        }
+        return data
+    }
+    func fetchData<T: Decodable>(for URLCase: NetworkURLCase ,type: T.Type) async throws -> T {
+        guard let url = components.getURLComponents(for: URLCase).url else {
+            throw(NetworkError.invalidURL)
+        }
+        let request = AF.request(url)
+        
+        let response = await request.serializingDecodable(T.self).response
+        guard (response.response)?.statusCode == 200 else {
+            throw NetworkError.serverError(code: response.response?.statusCode ?? 0)
+        }
+        
+        guard let data = response.value else {
+            throw NetworkError.decodingError
+        }
+        return data
     }
 }
 protocol DataDecodable {
@@ -73,8 +93,10 @@ class DecodeHandler: DataDecodable {
         }
     }
 }
+
 protocol URLComponentable {
     func getURLComponents() -> URLComponents
+    func getURLComponents(for type: NetworkURLCase) -> URLComponents
 }
 class URLComponentHandler: URLComponentable {
     let scheme = "https"
@@ -82,6 +104,8 @@ class URLComponentHandler: URLComponentable {
     let paths: [String] = ["B551011", "TarRlteTarService", "areaBasedList"]
     let mobileOS: String = "iOS"
     let mobileAppName: String = "KoreaTripRank"
+    let tripPaths: [String] = ["B551011", "TarRlteTarService", "areaBasedList"]
+    let weatherPaths: [String] = ["1360000","VilageFcstInfoService_2.0","getUltraSrtFcst"]
     
     func getURLComponents() -> URLComponents {
         var components = URLComponents()
@@ -99,6 +123,38 @@ class URLComponentHandler: URLComponentable {
             URLQueryItem(name: "signguCd", value: "50130"),
             URLQueryItem(name: "_type", value: "json")
         ]
+        return components
+    }
+    func getURLComponents(for type: NetworkURLCase) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        switch type {
+        case .trip:
+            components.path = "/" + tripPaths.joined(separator: "/")
+            components.percentEncodedQueryItems = [
+                URLQueryItem(name: "serviceKey", value: APIKEY().getKey()),
+                URLQueryItem(name: "numOfRows", value: "1"),
+                URLQueryItem(name: "MobileOS", value: mobileOS),
+                URLQueryItem(name: "MobileApp", value: mobileAppName),
+                URLQueryItem(name: "baseYm", value: "202409"),
+                URLQueryItem(name: "areaCd", value: "50"),
+                URLQueryItem(name: "signguCd", value: "50130"),
+                URLQueryItem(name: "_type", value: "json")
+            ]
+        case .weather:
+            components.path = "/" + weatherPaths.joined(separator: "/")
+            components.percentEncodedQueryItems = [
+                URLQueryItem(name: "serviceKey", value: APIKEY().getKey()),
+                URLQueryItem(name: "numOfRows", value: "30"),
+                URLQueryItem(name: "pageNo", value: "1"),
+                URLQueryItem(name: "dataType", value: "JSON"),
+                URLQueryItem(name: "base_date", value: "20241113"),
+                URLQueryItem(name: "base_time", value: "0059"),
+                URLQueryItem(name: "nx", value: "59"),
+                URLQueryItem(name: "ny", value: "125")
+            ]
+        }
         return components
     }
 }
