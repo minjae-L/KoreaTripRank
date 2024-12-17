b17572e55597b1585ed8f099dbaf1dc30211a0ce
이민재 <qlqlzbto@naver.com>
2024-12-17 22:18:22 +0900

    SearchViewModelTests 생성

---
+++ KoreaTripRank/KoreaTripRankTests/SearchViewModelTests.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewModelTests.swift
+//  KoreaTripRankTests
+//
+//  Created by 이민재 on 12/16/24.
+//
+
+import XCTest
+@testable import KoreaTripRank
+
+final class SearchViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+}
